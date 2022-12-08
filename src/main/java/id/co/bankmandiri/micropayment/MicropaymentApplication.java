85603f764606a4457c0e3f72ddbae35775e53b33
mcomacbook <mcomacbook@mcomacboksmbp43.supporting.corp.bankmandiri.co.id>
2022-12-08 09:19:04 +0700

    project initiated

---
+++ src/main/java/id/co/bankmandiri/micropayment/MicropaymentApplication.java
@@ -0,0 +1,13 @@
+package id.co.bankmandiri.micropayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicropaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicropaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bankmandiri/micropayment/MicropaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bankmandiri.micropayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicropaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
